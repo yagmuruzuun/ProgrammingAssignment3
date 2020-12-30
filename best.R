cfac242fbdf8fdc1889b429235d88658d8f6cbf8
Yagmur Uzun <75799867+yagmuruzuun@users.noreply.github.com>
2020-12-31 01:24:10 +0300

    Add files via upload

---
+++ best.R
@@ -0,0 +1,33 @@
+library(gtools)
+best <- function(state, outcome){
+data<- read.csv("outcome-of-care-measures.csv")
+p<-1
+if (!state %in% unique(data$State)){
+  p=0
+  stop("Invalid State")
+}
+if (!outcome %in% c("heart attack","heart failure","pneumonia")){
+  p=0
+  stop("Invalid Outcome")
+}
+if (p==1){
+  if (outcome=="heart attack"){
+    desired<-data[data$State==state,]
+    desired<-desired[mixedorder(desired$Hospital.Name),]
+    desired<-desired[desired$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==min(desired[,11]),]
+    return(desired[1,2])
+  }
+  if(outcome=="heart failure"){
+    desired<-data[data$State==state,]
+    desired<-desired[mixedorder(desired$Hospital.Name),]
+    desired<-desired[desired$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure==min(desired[,17]),]
+    return(desired[1,2])
+  }
+  if(outcome=="pneumonia"){
+    desired<-data[data$State==state,]
+    desired<-desired[mixedorder(desired$Hospital.Name),]
+    desired<-desired[desired$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia==min(desired[,23]),]
+    return(desired[1,2])
+  }
+}
+}
